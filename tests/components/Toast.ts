cbba641e6fcb53a8eb095ec0b2c2dda4898ff9a4
Fernanda <febaccarini@gmail.com>
2024-07-16 20:55:47 -0300

    feat toast component

---
+++ tests/components/Toast.ts
@@ -0,0 +1,19 @@
+import { expect, Page } from "@playwright/test";
+
+export class ToastComponent{
+    page: Page;
+    constructor(page){
+        this.page = page
+    }
+    
+    async haveText(text){
+        const toast = this.page.locator('div.toast')
+        await expect(toast).toHaveText(text);
+        await expect(toast).toBeHidden({timeout: 5000});
+        
+        // * Para conseguir pegar um elemento que some da tela
+        // await page.getByText('seus dados conosco').click();
+        // const content = await page.content();
+        // console.log(content)
+    }
+}
