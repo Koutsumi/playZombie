27c845bccbfe1f62f0939e8b8434d2acb73e50b1
Fernanda <febaccarini@gmail.com>
2024-07-25 21:09:44 -0300

    feat preenchimento do form de cadastro

--- tests/e2e/movies.spec.ts
+++ tests/e2e/movies.spec.ts
@@ -1,6 +1,25 @@
 import { test } from "@playwright/test";
+import { LoginPage } from "../pages/loginPage";
+import { ToastComponent } from "../components/Toast";
+import { AlertComponent } from "../components/Alert";
+import { MoviesPage } from "../pages/MoviesPage";
+import dataMovies from "../support/fixtures/movies.json"
+
+let loginPage
+let moviesPage
+let toastComponent
+let alertComponent
+
+test.beforeEach(({page}) => {
+    loginPage = new LoginPage(page);
+    moviesPage = new MoviesPage(page);
+    toastComponent = new ToastComponent(page);
+    alertComponent = new AlertComponent(page);
+})
 
 test("Deve poder cadastar um novo filme", async function({page}){
-    // é preciso estar logado
-    
+    await loginPage.visit();
+    await loginPage.submit("admin@zombieplus.com", "pwd123");
+    await moviesPage.isLoggedIn();
+    await moviesPage.create(dataMovies.guerra_mundial_z);
 })
--- tests/pages/MoviesPage.ts
+++ tests/pages/MoviesPage.ts
@@ -10,4 +10,23 @@ export class MoviesPage{
         await this.page.waitForLoadState('networkidle');
         await expect(this.page).toHaveURL(/.*admin/)
     }
+
+    async create(data){
+        await this.page.locator('a[href$="/movies/register"]').click();
+        await this.page.getByLabel(/Titulo do filme/).fill(data.title);
+        await this.page.getByLabel('Sinopse').fill(data.overview);
+
+        await this.page.locator('#select_company_id .react-select__indicator')
+            .click();
+        await this.page.locator('.react-select__option')
+            .filter({hasText:data.company})
+                .click();
+
+        await this.page.locator('#select_year .react-select__indicator')
+            .click();
+        await this.page.locator('.react-select__option')
+            .filter({hasText:data.release_year})
+                .click();
+        //console.log(await this.page.content())
+    }
 }
